8a5ba6e5f8a0470493f86bf8d586a2dce96823cd
jibrilbulama <40578703@live.napier.ac.uk>
2024-09-28 08:08:06 +0100

    Added more questions

--- index.js
+++ index.js
@@ -118,44 +118,184 @@ function clearStatusClass(element) {
   element.classList.remove('correct')
   element.classList.remove('wrong')
 }
+// Event listener for viewing the score on a separate page
+viewScoreButton.addEventListener('click', () => {
+    window.location.href = 'score.html' // Redirect to score page
+  })
 
 // Sample questions array
 const questions = [
-  {
-    question: 'What is 2 + 2?',
-    answers: [
-      { text: '4', correct: true },
-      { text: '22', correct: false }
-    ]
-  },
-  {
-    question: 'Who is the best Dev?',
-    answers: [
-      { text: 'Mr Jibril', correct: true },
-      { text: 'Mr Salim', correct: true },
-      { text: 'Mr Readone', correct: true },
-      { text: 'Mr Paul', correct: true }
-    ]
-  },
-  {
-    question: 'Is web development fun?',
-    answers: [
-      { text: 'Kinda', correct: false },
-      { text: 'YES!!!', correct: true },
-      { text: 'Um no', correct: false },
-      { text: 'IDK', correct: false }
-    ]
-  },
-  {
-    question: 'What is 4 * 2?',
-    answers: [
-      { text: '6', correct: false },
-      { text: '8', correct: true }
-    ]
-  }
-]
+    // Existing questions
+    {
+      question: 'What is 2 + 2?',
+      answers: [
+        { text: '4', correct: true },
+        { text: '22', correct: false }
+      ]
+    },
+    {
+      question: 'Who is the best YouTuber?',
+      answers: [
+        { text: 'Web Dev Simplified', correct: true },
+        { text: 'Traversy Media', correct: true },
+        { text: 'Dev Ed', correct: true },
+        { text: 'Fun Fun Function', correct: true }
+      ]
+    },
+    {
+      question: 'Is web development fun?',
+      answers: [
+        { text: 'Kinda', correct: false },
+        { text: 'YES!!!', correct: true },
+        { text: 'Um no', correct: false },
+        { text: 'IDK', correct: false }
+      ]
+    },
+    {
+      question: 'What is 4 * 2?',
+      answers: [
+        { text: '6', correct: false },
+        { text: '8', correct: true }
+      ]
+    },
+    {
+      question: 'What is the capital of France?',
+      answers: [
+        { text: 'Berlin', correct: false },
+        { text: 'Madrid', correct: false },
+        { text: 'Paris', correct: true },
+        { text: 'Lisbon', correct: false }
+      ]
+    },
+    {
+      question: 'Which planet is known as the Red Planet?',
+      answers: [
+        { text: 'Earth', correct: false },
+        { text: 'Mars', correct: true },
+        { text: 'Jupiter', correct: false },
+        { text: 'Venus', correct: false }
+      ]
+    },
+    {
+      question: 'What is the largest mammal?',
+      answers: [
+        { text: 'Elephant', correct: false },
+        { text: 'Blue Whale', correct: true },
+        { text: 'Giraffe', correct: false },
+        { text: 'Shark', correct: false }
+      ]
+    },
+    {
+      question: 'Which element has the chemical symbol O?',
+      answers: [
+        { text: 'Oxygen', correct: true },
+        { text: 'Gold', correct: false },
+        { text: 'Silver', correct: false },
+        { text: 'Hydrogen', correct: false }
+      ]
+    },
+    {
+      question: 'How many continents are there on Earth?',
+      answers: [
+        { text: '5', correct: false },
+        { text: '6', correct: false },
+        { text: '7', correct: true },
+        { text: '8', correct: false }
+      ]
+    },
+    // Avengers-themed questions
+    {
+      question: 'Which superhero is the leader of the Avengers in many versions of the team?',
+      answers: [
+        { text: 'Iron Man', correct: false },
+        { text: 'Captain America', correct: true },
+        { text: 'Thor', correct: false },
+        { text: 'Hulk', correct: false }
+      ]
+    },
+    {
+      question: 'In the Marvel Cinematic Universe, who is the first character to join the Avengers after the original six?',
+      answers: [
+        { text: 'Scarlet Witch', correct: true },
+        { text: 'Spider-Man', correct: false },
+        { text: 'Black Panther', correct: false },
+        { text: 'Vision', correct: false }
+      ]
+    },
+    {
+      question: 'Which villain is responsible for the Avengers coming together in the first Avengers movie?',
+      answers: [
+        { text: 'Ultron', correct: false },
+        { text: 'Thanos', correct: false },
+        { text: 'Loki', correct: true },
+        { text: 'Red Skull', correct: false }
+      ]
+    },
+    {
+      question: 'What is the name of the government organization that oversees the Avengers in the MCU?',
+      answers: [
+        { text: 'S.W.O.R.D.', correct: false },
+        { text: 'S.H.I.E.L.D.', correct: true },
+        { text: 'H.A.M.M.E.R.', correct: false },
+        { text: 'A.I.M.', correct: false }
+      ]
+    },
+    {
+      question: 'In the comics, which member of the Avengers is known for having a bow and arrow as their primary weapon?',
+      answers: [
+        { text: 'Black Widow', correct: false },
+        { text: 'Hawkeye', correct: true },
+        { text: 'Falcon', correct: false },
+        { text: 'Ant-Man', correct: false }
+      ]
+    },
+    // JavaScript-themed questions
+    {
+      question: 'Which keyword is used to declare a variable in JavaScript?',
+      answers: [
+        { text: 'var', correct: true },
+        { text: 'int', correct: false },
+        { text: 'float', correct: false },
+        { text: 'string', correct: false }
+      ]
+    },
+    {
+      question: 'What is the output of the expression "2" + 2 in JavaScript?',
+      answers: [
+        { text: '22', correct: true },
+        { text: '4', correct: false },
+        { text: 'NaN', correct: false },
+        { text: 'undefined', correct: false }
+      ]
+    },
+    {
+      question: 'Which of the following is not a JavaScript data type?',
+      answers: [
+        { text: 'Number', correct: false },
+        { text: 'String', correct: false },
+        { text: 'Boolean', correct: false },
+        { text: 'Character', correct: true }
+      ]
+    },
+    {
+      question: 'What does the isNaN() function do in JavaScript?',
+      answers: [
+        { text: 'Checks if a value is NaN', correct: true },
+        { text: 'Checks if a number is an integer', correct: false },
+        { text: 'Checks if a variable is defined', correct: false },
+        { text: 'Checks if a value is a number', correct: false }
+      ]
+    },
+    {
+      question: 'What is the correct syntax for referring to an external script called "script.js"?',
+      answers: [
+        { text: '<script src="script.js"></script>', correct: true },
+        { text: '<script href="script.js"></script>', correct: false },
+        { text: '<script ref="script.js"></script>', correct: false },
+        { text: '<script name="script.js"></script>', correct: false }
+      ]
+    }
+  ];
+  
 
-// Event listener for viewing the score on a separate page
-viewScoreButton.addEventListener('click', () => {
-  window.location.href = 'score.html' // Redirect to score page
-})
+
